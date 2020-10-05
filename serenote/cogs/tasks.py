c0116043f7ae633f8444377a308b61c1294ec0a9
LeptoFlare <47790183+LeptoFlare@users.noreply.github.com>
2020-10-05 04:25:27 +0000

    Create stub task cog

--- serenote/cogs/tasks.py
+++ serenote/cogs/tasks.py
@@ -1,18 +1,24 @@
 import discord
 from discord.ext import commands
 
+import ui
+
 
 class Tasks(commands.Cog):
     """Commands to managing tasks."""
 
     def __init__(self, bot):
         self.bot = bot
-    
-    @commands.command()
 
-    @commands.group(invoke_without_subcommand=True)
-    async def task()
+    @commands.group(invoke_without_command=True)
+    async def task(self, ctx, *, task):
+        """Create a new task."""
+        await ui.Alert(ctx, ui.Alert.Style.SUCCESS, title="New Task", description=task)
 
+    @task.command()
+    async def delete(self, ctx, *, task):
+        """Delete a task."""
+        await ui.Alert(ctx, ui.Alert.Style.DANGER, title="Task Deleted", description=task)
 
 def setup(bot):
     bot.add_cog(Tasks(bot))
