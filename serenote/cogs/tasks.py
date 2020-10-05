4644da7ed871ec9b51c925dc6bb9472d56c5f794
LeptoFlare <47790183+LeptoFlare@users.noreply.github.com>
2020-10-05 04:02:16 +0000

    Rename bot folder to serenote

--- serenote/cogs/tasks.py
+++ serenote/cogs/tasks.py
@@ -7,6 +7,11 @@ class Tasks(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+    
+    @commands.command()
+
+    @commands.group(invoke_without_subcommand=True)
+    async def task()
 
 
 def setup(bot):
